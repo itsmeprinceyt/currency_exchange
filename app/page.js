328c23687f50239af72eff3b5c03ee3d266bf996
itsmeprinceyt <114930668+itsmeprinceyt@users.noreply.github.com>
2024-08-10 23:29:18 +0530

    backend done

--- app/page.js
+++ app/page.js
@@ -1,15 +1,15 @@
 "use client";
 import { useState, useEffect } from 'react';
-import dotenv from "dotenv";
+import dotenv from "dotenv"; // so that I can access private environment variables
 export default function Home() {
-  const [baseCurrency, setbaseCurrency] = useState("USD");
-  const [targetCurrency, settargetCurrency] = useState("INR");
+  const [baseCurrency, setbaseCurrency] = useState("USD"); // default USD
+  const [targetCurrency, settargetCurrency] = useState("INR"); // default INR
   const [baseAmount, setbaseAmount] = useState("");
-  const [rate, setRate] = useState();
-  const [exchangeRateResult, setexchangeRateResult] = useState();
+  const [rate, setRate] = useState(); // storing current exchange rates for all currencies
+  const [exchangeRateResult, setexchangeRateResult] = useState(); // storing the final exchange rate entered by the user
 
-  useEffect(() => {
-    const ExchangeFetch = async () => {
+  useEffect(() => { // getting all the exchange rates on the first mount.
+    const ExchangeFetch = async () => { 
       let response = await fetch(process.env.NEXT_PUBLIC_APIURL);
       let jsonResponse = await response.json();
       setRate(jsonResponse.data);
@@ -17,27 +17,33 @@ export default function Home() {
     ExchangeFetch();
   }, [])
 
-  const finalExchange = (baseCurrency, targetCurrency, baseAmount) => {
-    const baseAmountInUSD = (baseAmount / rate[baseCurrency]).toFixed(3);
-    setexchangeRateResult(((baseAmountInUSD * rate[targetCurrency]).toFixed(3)));
+  const finalExchange = (baseCurrency, targetCurrency, baseAmount) => { // exchanging currency
+    const baseAmountInUSD = (baseAmount / rate[baseCurrency]).toFixed(3); // converting to USD since the exchange is based on USD only so we need to  convert the input to USD first and after that we can convert it to desired targetCurrency.
+    setexchangeRateResult(((baseAmountInUSD * rate[targetCurrency]).toFixed(3))); // using useState to set the result.
   }
 
-  const ExchangeData = async () => {
+  const ExchangeData = async () => { // fetching current exchange rate of all currencies.
     let response = await fetch(process.env.NEXT_PUBLIC_APIURL);
-    let jsonResponse = await response.json();
+    let jsonResponse = await response.json(); // converting into JSON format
     setRate(jsonResponse.data);
     finalExchange(baseCurrency, targetCurrency, baseAmount);
   }
 
-  const handleClick = () => {
-    console.log(`hit`)
+  const handleClick = () => { // Submit button
+    console.log(`I Am Here`)
     ExchangeData();
   }
+
+  const handleSwitch = () => { // Currency Switch
+    setbaseCurrency(targetCurrency);
+    settargetCurrency(baseCurrency);
+  }
+
   const currencyCodes = ['EUR', 'USD', 'JPY', 'BGN', 'CZK', 'DKK', 'GBP', 'HUF', 'PLN', 'RON', 'SEK', 'CHF', 'ISK', 'NOK', 'HRK', 'RUB', 'TRY', 'AUD', 'BRL', 'CAD', 'CNY', 'HKD', 'IDR', 'ILS', 'INR', 'KRW', 'MXN', 'MYR', 'NZD', 'PHP', 'SGD', 'THB', 'ZAR'];
   return (
     <div>
       <select onChange={(e) => setbaseCurrency(e.target.value)}>
-        <option value="USD">USD</option>
+        <option value={baseCurrency}>{baseCurrency}</option>
         {currencyCodes.map(currency => (
           <option key={currency} value={currency} >
             {currency}
@@ -45,7 +51,7 @@ export default function Home() {
         ))}
       </select>
       <select onChange={(e) => settargetCurrency(e.target.value)}>
-        <option value="INR">INR</option>
+        <option value={targetCurrency}>{targetCurrency}</option>
         {currencyCodes.map(currency => (
           <option key={currency} value={currency} >
             {currency}
@@ -60,7 +66,8 @@ export default function Home() {
         <input value={exchangeRateResult} disabled></input>
       </div>
       <div>
-        <button className="bg-sky-500 py-2 px-3 rounded-md shadow-md shadow-sky-500/30 text-white hover:opacity-75" onClick={() => { handleClick() }}>Submit</button>
+        <button className="animate-pulse delay-1000 px-3 y-2 bg-red-400 rounded-md shadow-md" onClick={handleSwitch}>|</button>
+        <button className="bg-sky-500 py-2 px-3 rounded-md shadow-md shadow-sky-500/30 text-white hover:opacity-75" onClick={handleClick}>Submit</button>
       </div>
     </div>
   );
